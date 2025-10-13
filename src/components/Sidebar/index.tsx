3821011103b4ddef25e5c316021bf59fbfc79653
Queren Sales <querenhsales@gmail.com>
2025-10-13 11:19:50 -0300

    🚧 Interface API
    definindo os dados

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,2 +1,13 @@
 import { useEffect, useState } from 'react';
 import styles from './Sidebar.module.css';
+
+interface Menu {
+  id: number;
+  label: string;
+  subMenus: SubMenu[];
+}
+
+interface SubMenu {
+  id: number;
+  label: string;
+}
