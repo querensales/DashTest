a853f95022ef1b6931fdd59cfff469b067f149ef
Queren Sales <querenhsales@gmail.com>
2025-10-13 11:57:53 -0300

    🚧 Loading...
    mensagem enquanto carrega

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,18 +15,23 @@ interface SubMenu {
 export default function Sidebar() {
     const [menus, setMenus] = useState<Menu[]>([]);
     const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/EnkiGroup/desafio-front-2025-2q/menus')
+            .then(response => response.json())
+            .then(receivedMenus=> {
+                setMenus(receivedMenus);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Erro ao buscar os menus:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <aside className={styles.sidebar}>Carregando menus...</aside>;
+    }
+
 
 }
-
-useEffect(() => {
-    fetch('https://my-json-server.typicode.com/EnkiGroup/desafio-front-2025-2q/menus')
-        .then(response => response.json())
-        .then(data => {
-            setMenus(data);
-            setLoading(false);
-        })
-        .cath(error => {
-            console.error("Erro ao buscar os menus:", error);
-            setLoading(false);
-        })
-}, []);
