a0f80d2dbabd4ac24cd0e43aa2f1f4b3c7963cf1
Queren Sales <querenhsales@gmail.com>
2025-10-14 20:03:58 -0300

    ⚡️  Efeito do hover
    quando o mouse estiver em cima do card

--- src/components/ItemCard/Index.tsx
+++ src/components/ItemCard/Index.tsx
@@ -1,17 +1,31 @@
 import type { Item } from '../../types';
 import styles from './itemCard.module.css';
 
+import { useState } from 'react';
+
 interface ItemCardProps {
     item: Item;
 }
 
 export function ItemCard({ item }: ItemCardProps) {
+    const [ismouseOnCard, setIsMouseOnCard] = useState(false);
+
     return (
-        <div className={styles.cardContainer}>
+        <div className={styles.cardContainer}
+            onMouseEnter={() => setIsMouseOnCard(true)}
+            onMouseLeave={() => setIsMouseOnCard(false)}>
+            {ismouseOnCard ? (
+                <input type="checkbox" className={styles.checkbox} />
+            ) : (
+                <div className={styles.ownerCircle}>
+                    {item.owner}
+                </div>
+            )}
+
             <div className={styles.ownerCircle}>
                 {item.owner}
             </div>
-            
+
             <div className={styles.mainContent}>
                 <span className={styles.name}>{item.name}</span>
                 <p className={styles.subject}>{item.subject}</p>
