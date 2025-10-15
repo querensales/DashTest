9f4d3b55313b57e38698a1496fa56efdf6f46421
Queren Sales <querenhsales@gmail.com>
2025-10-15 08:16:30 -0300

    🏗️ Elevando o Estado para a MainPage
    movendo toda a lógica de dados do ItemList para a MainPage.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,7 +3,12 @@ import { useState } from 'react';
 import styles from './MainPage.module.css';
 import { Sidebar } from '../../components/Sidebar';
 import { ItemList } from '../../components/ItemList';
+import type { Item } from '../../types';
 
+  const [items, setItems] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedSubMenuId, setSelectedSubMenuId] = useState<number | null>(null);
 
 export function MainPage() {
   const [selectedSubMenuId, setSelectedSubMenuId] = useState<number | null>(null);
