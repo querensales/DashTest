b2daaec4b07c3493857cb7d05f9d18491905b74a
Queren Sales <querenhsales@gmail.com>
2025-10-15 15:07:04 -0300

    ✨ Hook customizado
    para facilitar o uso e importa-lo aonde eu quiser

--- src/contexts/ChangeTheme.tsx
+++ src/contexts/ChangeTheme.tsx
@@ -31,3 +31,11 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
     );
 }
 
+export function useTheme() {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme deve ser usado dentro de um ThemeProvider');
+    }
+    return context;
+}
+
