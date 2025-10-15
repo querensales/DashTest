7a28f84a494467aeb5621fa638fbccdbfac5daa8
Queren Sales <querenhsales@gmail.com>
2025-10-15 16:42:20 -0300

    🚑️ Corrigindo código e linguagem
    fazendo correções

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import styles from './Header.module.css';
+import { useTheme } from '../../contexts/ChangeTheme'; 
+import { useTranslation } from 'react-i18next'; 
 
 interface HeaderProps {
     selectedItemsCount: number;
@@ -7,17 +9,29 @@ interface HeaderProps {
 
 export function Header({ selectedItemsCount, onArchive }: HeaderProps) {
     const hasSelection = selectedItemsCount > 0;
+    const { theme, toggleTheme } = useTheme();
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng: 'pt' | 'en') => {
+        i18n.changeLanguage(lng);
+    };
 
     return (
         <header className={styles.header}>
             <div>
                 <button onClick={onArchive} disabled={!hasSelection}>
-                    Arquivar
+                    {t('archive')}
                 </button>
-                {hasSelection && <span>{selectedItemsCount} item(s) selecionado(s)</span>}
+                {hasSelection && <span>{t('selected_items', { count: selectedItemsCount })}</span>}
             </div>
-            
-            {/*botão de logout */}
+            <div className={styles.controls}>
+                <button onClick={() => changeLanguage('pt')}>PT</button>
+                <button onClick={() => changeLanguage('en')}>EN</button>
+                
+                <button onClick={toggleTheme}>
+                    {theme === 'light' ? t('Mudar tema') : t('Mudar tema')}
+                </button>
+            </div>
         </header>
     );
 }
---
+++ src/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+const resources = {
+  en: {
+    translation: {
+      "archive": "Archive",
+      "selected_items": "{{count}} item(s) selected",
+      "change_to_dark_theme": "Switch to Dark Theme",
+      "change_to_light_theme": "Switch to Light Theme",
+    }
+  },
+
+  pt: {
+    translation: {
+      
+      "archive": "Arquivar",
+      "selected_items": "{{count}} item(s) selecionado(s)",
+      "change_to_dark_theme": "Mudar para Tema Escuro",
+      "change_to_light_theme": "Mudar para Tema Claro",
+    }
+  }
+};
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'pt',
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
